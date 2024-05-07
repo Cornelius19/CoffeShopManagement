41f5d15a83710e200a7bd3bbd4d6159f70d2da21
Cornelius19 <corneliu.museteanu1@gmail.com>
2024-05-07 18:57:07 +0300

    So the previous one does not work but pdfMake works just fine!

--- CoffeBarManagementAngular/src/app/orders/order-history/order-history.component.ts
+++ CoffeBarManagementAngular/src/app/orders/order-history/order-history.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, setTestabilityGetter } from '@angular/core';
 import { OrdersService } from '../orders.service';
 import { SharedService } from '../../shared/shared.service';
 import { OrderDto } from '../../shared/models/orderDto';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { style } from '@angular/animations';
+import { auto } from '@popperjs/core';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
     selector: 'app-order-history',
@@ -29,5 +35,38 @@ export class OrderHistoryComponent implements OnInit {
                 },
             });
         }
+    }
+
+    generateReceiptPDF(order: OrderDto) {
+        let totalSum: number = 0;
+        for(let prod of order.products){
+            totalSum = totalSum + (prod.quantity * prod.unitPrice);
+        }
+        const pageSize = {
+            width: 300, // specify width in points (1/72 inch)
+            height: auto, // specify height in points (1/72 inch)
+        };
+        let docDefinition = {
+            pageSize: pageSize,
+            content: [
+                { text: 'Order Note', style: 'header' },
+                { text: `Order ID: ${order.orderId}` },
+                { text: `Order date: ${order.orderDate}` },
+                { text: `Table ID: ${order.tableId}` },
+                { text: `Employee name: ${order.employeeName}` },
+                { text: `Order status: ${order.status}` },
+                { text: `Products:` },
+                {
+                    ul: order.products.map((prod) => [
+                        `Name:${prod.productName}`,  
+                        `Quan: ${prod.quantity}`,
+                        `Price: ${prod.unitPrice}`
+                    ]),
+                },
+                { text: `Total to pay: ${totalSum }$` },
+            ],
+        };
+
+        pdfMake.createPdf(docDefinition).open();
     }
 }
