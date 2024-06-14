88966dc63e5f36a65ec63f64da035d3ec8affda9
Cornelius19 <corneliu.museteanu1@gmail.com>
2024-06-14 21:13:01 +0300

    I finished the pos report on closing

--- CoffeBarManagement/CoffeBarManagement/obj/Debug/net8.0/CoffeBarManagement.AssemblyInfo.cs
+++ CoffeBarManagement/CoffeBarManagement/obj/Debug/net8.0/CoffeBarManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeBarManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43f587dc7d1e3482f8a0f5aa21b4f11e25ac0c8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be5a75df1865915cccb673dc1b7d4ecc8fb1b35")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeBarManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeBarManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CoffeBarManagementAngular/src/app/shared/shared.service.ts
+++ CoffeBarManagementAngular/src/app/shared/shared.service.ts
@@ -87,4 +87,32 @@ export class SharedService {
             },
         });
     }
+
+    getClosePOSData(date: string) {
+        return this.http.get(`${environment.appUrl}/api/pos/pos-closing-report/${date}`);
+    }
+
+    convertDateToYYMMDD(date: Date): string {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+
+    convertDateToDDMMYY(date: Date): string {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${day}-${month}-${year}`;
+    }
+
+    convertDateToYYMMDDHHMMSS(date: Date): string {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hour = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+        return `${day}-${month}-${year} ${hour}:${minutes}:${seconds}`;
+    }
 }
