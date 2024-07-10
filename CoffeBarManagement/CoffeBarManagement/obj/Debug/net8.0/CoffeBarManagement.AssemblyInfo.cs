381131f8a26946277d7fdea0e09c19ff44124ea7
Cornelius19 <corneliu.museteanu1@gmail.com>
2024-07-10 17:34:56 +0300

    something

--- CoffeBarManagement/CoffeBarManagement/obj/Debug/net8.0/CoffeBarManagement.AssemblyInfo.cs
+++ CoffeBarManagement/CoffeBarManagement/obj/Debug/net8.0/CoffeBarManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeBarManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d1b72e9887d101e6c7f0581b5c688c51b1dcf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd9a9438c326e215bf132e4632ac4c38d2fda2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeBarManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeBarManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
